7499975f66afaf774e838bfef0a4d9c538e15702
prudvi <patnamprudvinath2006@gmail.com>
2025-11-01 13:59:16 +0530

    deployement ready 2

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,9 +1,11 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '', // WARN: Use process.env for credentials in production!
+    host: process.env.DB_HOST_NAME,
+    user: process.env.DB_USER_NAME,
+    password: process.env.DB_PASSWORD,
     database: 'citrix',
     waitForConnections: true,
     connectionLimit: 10,
